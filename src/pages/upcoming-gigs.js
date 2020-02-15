5e942f99f266abb5b1ca1d759be80e6686382def
wjkw1 <wilsonw9378@gmail.com>
2020-02-15 15:42:13 +1300

    Added new navigation pages

---
+++ src/pages/upcoming-gigs.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Navigation from "../components/navigation"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const UpcomingGigsPage = () => (
+  <Layout>
+    <SEO title="Upcoming Gigs" />
+    <h1>Upcoming Gigs</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Navigation />
+  </Layout>
+)
+
+export default UpcomingGigsPage
