aefb89f34a0f835a8e90aa30e0b74efaf3823200
wjkw1 <wilsonw9378@gmail.com>
2020-03-05 21:19:20 +1300

    extracted socials into its own component

---
+++ src/components/socials.js
@@ -0,0 +1,19 @@
+// import { Link } from "gatsby"
+import React from "react"
+
+const Socials = () => {
+  return (
+    <>
+      <div>
+        <ul style={{ color: `white` }}>
+          <li>Spotify</li>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Socials
